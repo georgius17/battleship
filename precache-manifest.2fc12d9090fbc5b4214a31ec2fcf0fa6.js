52a6d8c2a9cbb8c8d9ca86fc50fc46ffb24d643a
Jiří Schejbal <jirkaschej@seznam.cz>
2020-07-13 18:34:10 +0200

    Updates

---
+++ precache-manifest.2fc12d9090fbc5b4214a31ec2fcf0fa6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7439f2385a9b818dcee9613218c46729",
+    "url": "/battleship/index.html"
+  },
+  {
+    "revision": "6e21bbc69fb8f1257510",
+    "url": "/battleship/static/css/main.e6ec2014.chunk.css"
+  },
+  {
+    "revision": "7f238937430d1c5968fb",
+    "url": "/battleship/static/js/2.ff5bcf45.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/battleship/static/js/2.ff5bcf45.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6e21bbc69fb8f1257510",
+    "url": "/battleship/static/js/main.055fe8d3.chunk.js"
+  },
+  {
+    "revision": "74e014da6bda1ba126c2",
+    "url": "/battleship/static/js/runtime-main.7d2909b7.js"
+  }
+]);
